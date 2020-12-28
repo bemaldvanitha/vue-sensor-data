625f0463f54d8d084746b9085fec1cd4e0414161
bemaldvanitha <bdvidanagamachchi@gmail.com>
2020-12-28 20:34:58 +0530

    select by id finish

--- src/views/SensorCharts.vue
+++ src/views/SensorCharts.vue
@@ -5,7 +5,12 @@
       <div class="col">
 
         <h2>{{ chatTitle }}</h2>
-        <line-chart :chartData="sensorData" :options="chartOptions" :label="chatTitle"></line-chart>
+
+        <select v-model="sensorId">
+            <option  v-for="sensor in sensorIdList" :value="sensor">{{ sensor }}</option>
+        </select>
+
+        <line-chart :chartData="changeGraphDataAccordingSelect" :options="chartOptions" :label="chatTitle"></line-chart>
       </div>
 
     </div>
@@ -30,27 +35,61 @@ export default {
         maintainAspectRatio: false
       },
       sensorData: [],
-      chatTitle: 'Temperature * c'
+      chatTitle: 'Temperature * c',
+      sensorId: '',
+      sensorIdList: [],
     };
+  },
+  computed: {
+    changeGraphDataAccordingSelect(){
+
+      if(this.sensorId === 'all'){
+        return this.sensorData;
+      }else{
+        return this.sensorData.filter(data => data.sensorId !== this.sensorId);
+      }
+
+    }
   },
   created()  {
     axios.get(`http://localhost:5000/temperature/data`).then(response => {
       const data = response.data;
 
+      this.sensorIdList.push('all');
+
       data.forEach(singleData => {
         const date = moment(singleData.date,"YYYYMMDD").format("MM/DD");
         const value = singleData.data_value;
+        const sensorId = singleData.sensor_id;
+
+        if(!this.sensorIdList.includes(singleData.sensor_id)){
+          this.sensorIdList.push(singleData.sensor_id);
+        }
 
         this.sensorData.push({
           date: date,
-          value: value
+          value: value,
+          sensorId: sensorId
         });
-      })
+
+      });
+      this.sensorId = this.sensorIdList[0];
     });
   }
 }
 </script>
 
 <style>
-
+  input , select{
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  option {
+    color: #ff3f80;
+  }
 </style>
